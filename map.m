c6236452283388ebc669884a208c26608015cf08
feimengjuan <1120011573@qq.com>
2019-03-05 19:51:01 +0800

    Add files via upload

--- map.m
+++ map.m
@@ -1,10 +1,10 @@
 % 原始图像，需要进行灰度化，我这里直接读灰度图
-im = imread('2011_09_26_drive_0079_sync_image_0000000035_image_03.png');
+im = imread('2011_10_03_drive_0047_sync_image_0000000185_image_02.png');
 img = rgb2gray(im);
 im = double(img);
  
 % 概率图扩展范围是0-255
-gray = depth_read('2011_09_26_drive_0079_sync_image_0000000035_image_03_pred.png');
+gray = depth_read('2011_10_03_drive_0047_sync_image_0000000185_image_02_pred.png');
 gray = uint8(gray);
  
 % jet定义颜色，红-蓝：255-0，可以设置jet(阈值)调整210为红色
